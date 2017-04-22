b89ad40460b66314523050b102c025e5a2cac361
Kirk <kirk@kirkwarez.com>
2017-04-22 11:11:47 +0200

    Tracked additional filetypes, unignored binaries (so artists don't need Visual Studio), set up basic project, created map, created first blueprint stuff (planetoid generator and camera controls), created model asset.

---
+++ Projects/PestControl/Source/PestControl/PestControlGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PestControl.h"
+#include "PestControlGameModeBase.h"
+
+
+
+
---
+++ Projects/PestControl/Source/PestControl/PestControlGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PestControlGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PESTCONTROL_API APestControlGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
