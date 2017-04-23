89f6422c57f417afe4b77581ca9328a5531083df
Kirk <kirk@kirkwarez.com>
2017-04-23 21:22:24 +0200

    Added setup for lokators, showed cursor, made rotation with right mouse button.

---
+++ Projects/PestControl/Source/PestControl/LokatorBase.cpp
@@ -0,0 +1,28 @@
+// Copyright Echo Team 2017
+
+#include "PestControl.h"
+#include "LokatorBase.h"
+
+
+// Sets default values
+ALokatorBase::ALokatorBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALokatorBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALokatorBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Projects/PestControl/Source/PestControl/LokatorBase.h
@@ -0,0 +1,27 @@
+// Copyright Echo Team 2017
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "LokatorBase.generated.h"
+
+UCLASS()
+class PESTCONTROL_API ALokatorBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ALokatorBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
